80355761f314c1bcea46af98623ccc60bdaa8d4a
anamika-2408 <78497091+anamika-2408@users.noreply.github.com>
2022-11-30 21:32:34 +0530

    Add files via upload

---
+++ DbConnection.kt
@@ -0,0 +1,10 @@
+
+import java.sql.Connection
+import java.sql.DriverManager
+class DbConnection{
+    fun connect(): Connection {
+        val myurl="jdbc:mysql://localhost:3306/kotlindb"
+        val connection= DriverManager.getConnection(myurl,"root","mrudula")
+        return connection
+    }
+}
